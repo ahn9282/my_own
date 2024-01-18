cc418d86801d4b2417dc54a07a2f1becc2274f61
tjqwn <tjqwn@ahn-laptop>
2024-01-18 16:58:26 +0900

    add

---
+++ my_own/src/justFUN/LogIn_Console_Swing.java
@@ -0,0 +1,53 @@
+package justFUN;
+
+
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+public class LogIn_Console_Swing extends JFrame{
+
+	
+	
+	LogIn_Console_Swing() {
+		setTitle("Please Sign-in!!");
+		JPanel p = new JPanel(new FlowLayout()); 
+		p.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		JLabel ID = new JLabel("ID : ");
+		JLabel PW = new JLabel("PW : ");
+		JTextArea tID = new JTextArea(1,8);
+		JTextArea tPW = new JTextArea(1,8);
+		JButton b1 = new JButton("Log-In");
+		b1.setBounds(200, 200, 60, 40);
+		p.add(ID);
+		p.add(tID);
+		p.add(PW);
+		p.add(tPW);
+		p.add(b1);
+		add(p);
+		ID.setForeground(Color.white);// ID set color to white
+		ID.getForeground();              // return ID's text color to white
+		PW.setForeground(Color.white);
+		PW.getForeground();
+		p.setBackground(Color.black);// p set color to black
+		p.getBackground();             // return p's text color to black
+			
+			setSize(600, 200);//width = 600, height = 100
+			//pack();
+			setVisible(true);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		}
+	
+	
+	public static void main(String[] args) {
+		new LogIn_Console_Swing();
+	}
+	}
+
