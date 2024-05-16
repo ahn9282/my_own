e13f5ba1bb5f9d1c8b4ea142e17d66eb8c6b2166
cajunSub <junsyub@gmail.com>
2024-05-16 19:29:03 +0900

    Algorithm study Initial Commit

---
+++ src/BaekJoon/gold/BjTemplate.java
@@ -0,0 +1,13 @@
+package BaekJoon.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class BjTemplate {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
---
+++ src/BaekJoon/silver/Bj11659.java
@@ -0,0 +1,13 @@
+package BaekJoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Bj11659 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
